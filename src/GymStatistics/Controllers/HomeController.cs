8639d8ba92ab3458b29bd21811a33e646626d4c3
Alexandr Pavlovski <a.pavlovski@paralect.com>
2019-02-21 22:18:42 +0300

    implemented test google authorization

--- src/GymStatistics/Controllers/HomeController.cs
+++ src/GymStatistics/Controllers/HomeController.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Services;
+
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util;
+using Newtonsoft.Json;
 
 namespace GymStatistics.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index(CancellationToken cancellationToken)
         {
-            return View();
+            var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).AuthorizeAsync(cancellationToken);
+
+            if (result.Credential != null)
+            {
+                var service = new SheetsService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = result.Credential,
+                    ApplicationName = "gym-statistics"
+                });
+
+                string id = "18JWctnEO-3gTZ68-WLUkxrQyulycOPxgwvqmHUrGCsI";
+                string range = "Sheet1!A2:E";
+                ValueRange valueRange = new ValueRange();
+                valueRange.MajorDimension = "COLUMNS";//"ROWS";//COLUMNS
+
+                var request = service.Spreadsheets.Values.Get(id, range);
+                var vals = request.Execute();
+
+                return View(model: JsonConvert.SerializeObject(vals));
+            }
+            else
+            {
+                return new RedirectResult(result.RedirectUri);
+            }
         }
 
         public ActionResult About()
@@ -25,6 +55,34 @@ namespace GymStatistics.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public async Task<ActionResult> Goo(CancellationToken cancellationToken)
+        {
+            var result = await new AuthorizationCodeMvcApp(this, new AppFlowMetadata()).AuthorizeAsync(cancellationToken);
+
+            if (result.Credential != null)
+            {
+                var service = new SheetsService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = result.Credential,
+                    ApplicationName = "gym-statistics"
+                });
+
+                string id = "18JWctnEO-3gTZ68-WLUkxrQyulycOPxgwvqmHUrGCsI";
+                string range = "Sheet1!A2:E";
+                ValueRange valueRange = new ValueRange();
+                valueRange.MajorDimension = "COLUMNS";//"ROWS";//COLUMNS
+
+                var request = service.Spreadsheets.Values.Get(id, range);
+                var vals = request.Execute();
+
+                return View();
+            }
+            else
+            {
+                return new RedirectResult(result.RedirectUri);
+            }
         }
     }
 }
